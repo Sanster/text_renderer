073004af5216130c8132be7e38e9d9d4c2855de7
sanster <cwq1913@gmail.com>
2018-07-13 17:12:28 +0800

    add test code to test seamlessClone

---
+++ tools/test.py
@@ -0,0 +1,50 @@
+from PIL import ImageFont, Image, ImageDraw
+import numpy as np
+import cv2
+
+bg_path = '/home/cwq/code/ocr_end2end/text_gen/bg/000000088218.jpg'
+font_path = '/home/cwq/code/ocr_end2end/text_gen/fonts/chn/msyh.ttc'
+
+word = '测试图像的无缝融合'
+font = ImageFont.truetype(font_path, 25)
+offset = font.getoffset(word)
+size = font.getsize(word)
+
+print(offset)
+word_height = size[1] - offset[1]
+word_width = size[0]
+
+# bg = np.ones((size[1] - offset[1], size[0] - offset[0]), np.uint8) * 255
+bg = cv2.imread(bg_path)
+bg_gray = cv2.cvtColor(bg, cv2.COLOR_BGR2GRAY)
+bg_height = bg.shape[0]
+bg_width = bg.shape[1]
+
+white_bg = np.ones((word_height + 1, word_width)) * 255
+text_img = Image.fromarray(np.uint8(white_bg))
+draw = ImageDraw.Draw(text_img)
+draw.text((0, 0 - offset[1]), word, fill=150, font=font)
+
+bg_gray = Image.fromarray(np.uint8(bg_gray))
+graw_draw = ImageDraw.Draw(bg_gray)
+graw_draw.text((bg_width // 2 - word_width // 2, bg_height // 2 - word_height // 2), word, fill=150, font=font)
+bg_gray.save('direct.jpg')
+
+text_img.save('text.jpg')
+text_img = np.array(text_img).astype(np.uint8)
+
+text_mask = 255 * np.ones(text_img.shape, text_img.dtype)
+
+# This is where the CENTER of the airplane will be placed
+center = (bg_width // 2, bg_height // 2)
+
+text_img_bgr = np.ones((text_img.shape[0], text_img.shape[1], 3), np.uint8)
+cv2.cvtColor(text_img, cv2.COLOR_GRAY2BGR, text_img_bgr)
+
+print(text_img_bgr.shape)
+print(bg.shape)
+print(text_mask.shape)
+mixed_clone = cv2.seamlessClone(text_img_bgr, bg, text_mask, center, cv2.MIXED_CLONE)
+
+result = cv2.cvtColor(mixed_clone, cv2.COLOR_BGR2GRAY)
+cv2.imwrite('test.jpg', result)
