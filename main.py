c0d70d418e9e76f6595d1f5c6842cbda89150818
guoxiaolu <guoxiaolu06@126.com>
2020-10-12 19:52:41 +0800

    add test

--- main.py
+++ main.py
@@ -22,7 +22,8 @@ import cv2
 
 from libs.config import load_config
 from libs.timer import Timer
-from parse_args import parse_args
+# from parse_args import parse_args
+from parse_args_en import parse_args
 import libs.utils as utils
 import libs.font_utils as font_utils
 from textrenderer.corpus.corpus_utils import corpus_factory
@@ -36,11 +37,13 @@ STOP_TOKEN = 'kill'
 flags = parse_args()
 cfg = load_config(flags.config_file)
 
+corpus = corpus_factory(flags.corpus_mode, flags.chars_file, flags.corpus_dir, flags.length, flags.max_length)
+
 fonts = font_utils.get_font_paths_from_list(flags.fonts_list)
 bgs = utils.load_bgs(flags.bg_dir)
 
-corpus = corpus_factory(flags.corpus_mode, flags.chars_file, flags.corpus_dir, flags.length)
-
+if flags.num_img == -1:
+    flags.num_img = len(corpus.corpus)
 renderer = Renderer(corpus, fonts, bgs, cfg,
                     height=flags.img_height,
                     width=flags.img_width,
@@ -74,7 +77,7 @@ def gen_img_retry(renderer, img_index):
     try:
         return renderer.gen_img(img_index)
     except Exception as e:
-        print("Retry gen_img: %s" % str(e))
+        print("Retry gen_img: %s, img_index: %s" % (str(e), img_index))
         traceback.print_exc()
         raise Exception
 
@@ -85,26 +88,30 @@ def generate_img(img_index, q=None):
     np.random.seed()
 
     im, word = gen_img_retry(renderer, img_index)
+    is_large = False
+    if im.shape[1] > flags.img_max_width:
+        is_large = True
 
     base_name = '{:08d}'.format(img_index)
 
     if not flags.viz:
-        fname = os.path.join(flags.save_dir, base_name + '.jpg')
-        cv2.imwrite(fname, im)
+        if not is_large:
+            fname = os.path.join(flags.save_dir, base_name + '.jpg')
+            cv2.imwrite(fname, im)
 
-        label = "{} {}".format(base_name, word)
+            label = "{} {}".format(base_name, word)
 
-        if q is not None:
-            q.put(label)
+            if q is not None:
+                q.put(label)
 
-        with lock:
-            counter.value += 1
-            print_end = '\n' if counter.value == flags.num_img else '\r'
-            if counter.value % 100 == 0 or counter.value == flags.num_img:
-                print("{}/{} {:2d}%".format(counter.value,
-                                            flags.num_img,
-                                            int(counter.value / flags.num_img * 100)),
-                      end=print_end)
+            with lock:
+                counter.value += 1
+                print_end = '\n' if counter.value == flags.num_img else '\r'
+                if counter.value % 100 == 0 or counter.value == flags.num_img:
+                    print("{}/{} {:2d}%".format(counter.value,
+                                                flags.num_img,
+                                                int(counter.value / flags.num_img * 100)),
+                        end=print_end)
     else:
         utils.viz_img(im)
 
@@ -154,7 +161,7 @@ if __name__ == "__main__":
     manager = mp.Manager()
     q = manager.Queue()
 
-    start_index = restore_exist_labels(label_path)
+    start_index = restore_exist_labels(tmp_label_path)
 
     timer = Timer(Timer.SECOND)
     timer.start()
